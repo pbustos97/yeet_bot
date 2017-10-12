bb3f8148fbbba8b692993c2e91b916e20013fcb0
Patrick Bustos <saintpat97@gmail.com>
2017-10-11 21:41:42 -0700

    removed unwanted commands and fixed kaz command

--- yeet_bot.py
+++ yeet_bot.py
@@ -96,18 +96,6 @@ class Music:
                 pass
 
     @commands.command(pass_context=True, no_pm=True)
-    async def join(self, ctx, *, channel : discord.Channel):
-        """Joins a voice channel."""
-        try:
-            await self.create_voice_client(channel)
-        except discord.ClientException:
-            await self.bot.say('Already in a voice channel...')
-        except discord.InvalidArgument:
-            await self.bot.say('This is not a voice channel...')
-        else:
-            await self.bot.say('Ready to play audio in ' + channel.name)
-
-    @commands.command(pass_context=True, no_pm=True)
     async def summon(self, ctx):
         """Summons the bot to join your voice channel."""
         summoned_channel = ctx.message.author.voice_channel
@@ -124,37 +112,6 @@ class Music:
         return True
 
     @commands.command(pass_context=True, no_pm=True)
-    async def play(self, ctx, *, song : str):
-        """Plays a song.
-        If there is a song currently in the queue, then it is
-        queued until the next song is done playing.
-        This command automatically searches as well from YouTube.
-        The list of supported sites can be found here:
-        https://rg3.github.io/youtube-dl/supportedsites.html
-        """
-        state = self.get_voice_state(ctx.message.server)
-        opts = {
-            'default_search': 'auto',
-            'quiet': True,
-        }
-
-        if state.voice is None:
-            success = await ctx.invoke(self.summon)
-            if not success:
-                return
-
-        try:
-            player = await state.voice.create_ytdl_player(song, ytdl_options=opts, after=state.toggle_next)
-        except Exception as e:
-            fmt = 'An error occurred while processing this request: ```py\n{}: {}\n```'
-            await self.bot.send_message(ctx.message.channel, fmt.format(type(e).__name__, e))
-        else:
-            player.volume = 0.6
-            entry = VoiceEntry(ctx.message, player)
-            await self.bot.say('Enqueued ' + str(entry))
-            await state.songs.put(entry)
-
-    @commands.command(pass_context=True, no_pm=True)
     async def volume(self, ctx, value : int):
         """Sets the volume of the currently playing song."""
 
@@ -163,22 +120,6 @@ class Music:
             player = state.player
             player.volume = value / 100
             await self.bot.say('Set the volume to {:.0%}'.format(player.volume))
-
-    @commands.command(pass_context=True, no_pm=True)
-    async def pause(self, ctx):
-        """Pauses the currently played song."""
-        state = self.get_voice_state(ctx.message.server)
-        if state.is_playing():
-            player = state.player
-            player.pause()
-
-    @commands.command(pass_context=True, no_pm=True)
-    async def resume(self, ctx):
-        """Resumes the currently played song."""
-        state = self.get_voice_state(ctx.message.server)
-        if state.is_playing():
-            player = state.player
-            player.resume()
 
     @commands.command(pass_context=True, no_pm=True)
     async def stop(self, ctx):
@@ -198,70 +139,44 @@ class Music:
             await state.voice.disconnect()
         except:
             pass
-
-    @commands.command(pass_context=True, no_pm=True)
-    async def skip(self, ctx):
-        """Vote to skip a song. The song requester can automatically skip.
-        3 skip votes are needed for the song to be skipped.
-        """
-
-        state = self.get_voice_state(ctx.message.server)
-        if not state.is_playing():
-            await self.bot.say('Not playing any music right now...')
-            return
-
-        voter = ctx.message.author
-        if voter == state.current.requester:
-            await self.bot.say('Requester requested skipping song...')
-            state.skip()
-        elif voter.id not in state.skip_votes:
-            state.skip_votes.add(voter.id)
-            total_votes = len(state.skip_votes)
-            if total_votes >= 3:
-                await self.bot.say('Skip vote passed, skipping song...')
-                state.skip()
-            else:
-                await self.bot.say('Skip vote added, currently at [{}/3]'.format(total_votes))
-        else:
-            await self.bot.say('You have already voted to skip this song.')
-
-    @commands.command(pass_context=True, no_pm=True)
-    async def playing(self, ctx):
-        """Shows info about the currently played song."""
-
-        state = self.get_voice_state(ctx.message.server)
-        if state.current is None:
-            await self.bot.say('Not playing anything.')
-        else:
-            skip_count = len(state.skip_votes)
-            await self.bot.say('Now playing {} [skips: {}/3]'.format(state.current, skip_count))
     
     @commands.command(pass_context=True, no_pm=True)
-    async def kaz(self,ctx,channel: discord.Channel):
-        voice = await bot.join_voice_channel(channel)
+    async def kaz(self,ctx):
+        summoned_channel = ctx.message.author.voice_channel
+        if summoned_channel is None:
+            await self.bot.say('You are not in a voice channel.')
+            return False
+        state = self.get_voice_state(ctx.message.server)
+        if state.voice is None:
+            state.voice = await self.bot.join_voice_channel(summoned_channel)
+        else:
+            await state.voice.move_to(summoned_channel)
+        voice = state.voice
         player = voice.create_ffmpeg_player('kaz.mp3')
+        player.volume = 0.5
         player.start()
 
+        return True
 
 bot = commands.Bot(command_prefix=commands.when_mentioned_or('$'), description='A playlist example for discord.py')
 bot.add_cog(Music(bot))
 
-@bot.event
-async def on_message(message):
-    if message.content.startswith('!shutup'):
-        await bot.send_message(message.channel, '@SVINT#4084 shut up.')
-    elif message.content.startswith('yah'):
-        await bot.send_message(message.channel, 'yeet')
-    elif message.content.startswith('!ting'):
-        await bot.send_message(message.channel, 'the ting go skrrra! pa pa ka ka ka!')
-    elif message.content.startswith('hol up'):
-        await bot.send_message(message.channel, 'we dem boyz')
-        time.sleep(1)
-        await bot.send_message(message.channel, 'we makin noise')
-    elif message.content.startswith('stfu'):
-        await bot.send_message(message.channel, 'ok')
-    elif message.content.startswith('patriots'):
-        await bot.send_message(message.channel, 'play video')
+#@bot.event
+#async def on_message(message):
+#    if message.content.startswith('!shutup'):
+#        await bot.send_message(message.channel, '@SVINT#4084 shut up.')
+#    elif message.content.startswith('yah'):
+#        await bot.send_message(message.channel, 'yeet')
+#    elif message.content.startswith('!ting'):
+#        await bot.send_message(message.channel, 'the ting go skrrra! pa pa ka ka ka!')
+#    elif message.content.startswith('hol up'):
+#        await bot.send_message(message.channel, 'we dem boyz')
+#        time.sleep(1)
+#        await bot.send_message(message.channel, 'we makin noise')
+#    elif message.content.startswith('stfu'):
+#        await bot.send_message(message.channel, 'ok')
+#    elif message.content.startswith('patriots'):
+#        await bot.send_message(message.channel, 'play video')
 
 @bot.event
 async def on_ready():
